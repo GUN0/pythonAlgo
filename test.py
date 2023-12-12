b90ac624aff87ad0bae580abb017af2c5146cc1e
gun <safocum@gmail.com>
2023-12-12 23:19:01 +0300

    p&l update

--- test.py
+++ test.py
@@ -2,11 +2,17 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 
 class Signal:
-    def __init__(self, balance):
+    def __init__(self, balance,cnst_blnc):
         self.states = ['buy', 'sell', 'none']
         self.state = 'none'
         self.balance = balance
         self.lotsize = None
+        # Get Initial Balance as a Constant For Profit Loss Calculation Difference
+        self.cnst_blnc = cnst_blnc
+        # Holder is Used to Store the Profit Loss Data 
+        self.holder = 0
+        # Holder2 is Used for storing Updated Balance (If Used the self.balance instead the P&L Ratio gets Affected Poorly)
+        self.holder2 = cnst_blnc
     
     def buy_order(self, price_buy):
         if self.state == 'none' or self.state == 'sell':
@@ -14,8 +20,7 @@ class Signal:
             self.state = 'buy'
             self.lotsize = self.balance // price_buy
             print('Lot Size:', int(self.lotsize))
-            # rest = self.balance - lotsize * price_buy
-            self.balance = self.balance - self.lotsize * price_buy
+            self.holder2 = self.balance - self.lotsize * price_buy
 
         else:
             None
@@ -24,8 +29,10 @@ class Signal:
         if self.state == 'buy':
             print('Sold at: {:.2f}'.format(price_sell))
             self.state = 'sell'
-            self.balance = self.balance + self.lotsize * price_sell
+            self.balance = self.holder2 + self.lotsize * price_sell
+            self.holder = self.balance - self.cnst_blnc
             print('Balance: {:.2f}'.format(self.balance))
+            print("P&L = {:.2f}".format(self.holder))
             print('\n')
 
         else:
@@ -37,17 +44,22 @@ class Signal:
     def get_balance(self):
         return self.balance
 
+    def get_cnst_blnc(self):
+        return self.holder
+
+
 initial_balance = 10000
-signal = Signal(balance=initial_balance)
+int_blc2 = initial_balance
+pnl = []
+signal = Signal(balance=initial_balance, cnst_blnc=int_blc2)
 symbol = "ANSGR.IS"
 start_date = "2022-01-01"
 end_date = "2023-01-01"
 sma1 = 8
 sma2 = 21
+balance_data = []
 
 data = yf.download(symbol, start=start_date, end=end_date)
-# close_price = data['Adj Close']
-# print(close_price)
 
 data['SMA1'] = data['Adj Close'].rolling(window=sma1).mean()
 data['SMA2'] = data['Adj Close'].rolling(window=sma2).mean()
@@ -60,7 +72,10 @@ for i in range(len(data)):
     elif data['SMA1'].iloc[i] < data['SMA2'].iloc[i]:
         signal.sell_order(data['Adj Close'].iloc[i])
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+    balance_data.append(signal.get_balance())
+    pnl.append(signal.get_cnst_blnc())
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 10), sharex=True)
 
 ax1.plot(data['Adj Close'], label = symbol + ' Price', linewidth=3)
 ax1.plot(data['SMA1'], label=f'{sma1} Day Moving Average', color='green')
@@ -69,27 +84,16 @@ ax1.set_title(f'{symbol} Stock Price With Moving Averages')
 ax1.set_ylabel('Price (USD)')
 ax1.legend()
 
-# balance_data = [initial_balance - signal.get_balance() for _ in range(len(data))]
-for _ in range(len(data)):
-    print(signal.get_balance())
-# print(signal.get_balance())
-# ax2.plot(data.index, balance_data, label='Balance', linewidth=2, color='red')
+ax2.plot(data.index, balance_data, label='Balance', linewidth=2, color='red')
 ax2.set_xlabel('Date')
 ax2.set_ylabel('Balance (USD)')
-ax1.legend()
+ax2.legend()
+
+ax3.plot(data.index, pnl, label='P&L', linewidth=2, color='red')
+ax3.set_xlabel('Date')
+ax3.set_ylabel('P&L (USD)')
+ax3.legend()
 
 plt.show()
 
 
-
-
-# plt.figure(figsize=(10, 6))
-# plt.plot(data['Adj Close'], label=symbol + ' Price', linewidth=3)
-# plt.plot(data['SMA1'], label=f'{sma1} Day Moving Average', color='green')
-# plt.plot(data['SMA2'], label=f'{sma2} Day Moving Average', color='red')
-# plt.title(f'{symbol} Stock Price With Moving Averages')
-# plt.xlabel('Date')
-# plt.ylabel('Price (USD)')
-# plt.legend()
-#
-# plt.show()
